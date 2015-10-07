dbef284cc49f79bfc555b3b6028a3f15c55913e4
[rachyllmorgan] <[rachyllmorgan@gmail.com]>
2015-10-06 19:47:34 -0500

    started API

--- app/controllers/display-songs.js
+++ app/controllers/display-songs.js
@@ -4,16 +4,27 @@ app.controller('SongCtrl',
 	"$firebaseArray",
 	"$location",
 	"simple-storage", 
+  "$http",
 	
-	function($scope, $firebaseArray, $location, simple_storage) {
+	function($scope, $firebaseArray, $location, simple_storage, $http) {
 
 		$scope.songs=[];
 
-	  var ref = new Firebase("https://radiant-fire-6211.firebaseio.com/songs");
-  		// create a synchronized array
-  		$scope.songs = $firebaseArray(ref);
+	 //  var ref = new Firebase("https://radiant-fire-6211.firebaseio.com/songs");
+  // 		// create a synchronized array
+  // 		$scope.songs = $firebaseArray(ref);
 
-  console.log("calling get_songs", $scope.songs);
+  // console.log("calling get_songs", $scope.songs);
+
+
+
+  // $scope.search = function(){
+  //   console.log("$scope.userInput", $scope.userInput);
+    $http.get('http://freemusicarchive.org/api/get/{dataset}.{format}?api_key=').
+    then(function(data) {///search?q=Goosinator&type=beer
+      console.log(data);
+
+    });
 
   $scope.showSongDetail = function (song){
   	
